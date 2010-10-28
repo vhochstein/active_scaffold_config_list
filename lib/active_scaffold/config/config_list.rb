910656966507111ff4d90e24fadbf498ec08c005
vhochstein <v.hochstein@highstone.de>
2010-10-28 12:45:57 +0200

    use localization for config_list label

--- lib/active_scaffold/config/config_list.rb
+++ lib/active_scaffold/config/config_list.rb
@@ -24,7 +24,7 @@ module ActiveScaffold::Config
     # ----------------------------
     # the label= method already exists in the Form base class
     def label
-      @label ? as_(@label) : as_('Config list %{model} columns', :model => @core.label.singularize)
+      @label ? as_(@label) : as_(:config_list_model, :model => @core.label.singularize)
     end
 
     # if you do not want to show all columns as a default you may define same
