693003c176f79ea6b2fd6ba0957310fb5d729da9
vhochstein <v.hochstein@highstone.de>
2012-02-11 18:13:09 +0100

    use engine initializer

--- lib/active_scaffold_config_list.rb
+++ lib/active_scaffold_config_list.rb
@@ -1,11 +1,5 @@
-# Make sure that ActiveScaffold has already been included
-ActiveScaffold rescue throw "should have included ActiveScaffold plug in first.  Please make sure that this overwrite plugging comes alphabetically after the ActiveScaffold plug in"
-
 require 'active_scaffold_config_list/engine'
 require 'active_scaffold_config_list/version'
-
-# Load our overrides
-require "active_scaffold_config_list/config/core.rb"
 
 module ActiveScaffoldConfigList
   def self.root
@@ -26,5 +20,3 @@ module ActiveScaffold
     ActiveScaffold.autoload_subdir('helpers', self, File.dirname(__FILE__))
   end
 end
-
-ActionView::Base.send(:include, ActiveScaffold::Helpers::ConfigListHelpers)
--- lib/active_scaffold_config_list/engine.rb
+++ lib/active_scaffold_config_list/engine.rb
@@ -3,6 +3,15 @@ module ActiveScaffoldConfigList
   # not sure if it is a must though...
   #module Rails
     class Engine < ::Rails::Engine
+      initializer("initialize_active_scaffold_config_list", :after => "initialize_active_scaffold") do
+        ActiveSupport.on_load(:action_controller) do
+          require "active_scaffold_config_list/config/core.rb"
+        end
+      end
+
+      ActiveSupport.on_load(:action_view) do
+        include ActiveScaffold::Helpers::ConfigListHelpers
+      end
     end
   #end
 end
